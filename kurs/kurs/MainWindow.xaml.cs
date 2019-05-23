c864523720fda2f892b7c8841257e9d2570ee615
juziem <lus0800@mail.ru>
2019-05-23 14:06:04 +0700

    add move

---
+++ kurs/kurs/MainWindow.xaml.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kurs
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class CChar
+        {
+            int x, y;
+            int px, py;
+
+            int h, w;
+            Ellipse pm;
+
+            public CChar(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+
+                w = h = 45;
+
+                px = x * w; 
+                py = y * h;
+
+                pm = new Ellipse();
+                //...
+            }
+
+            public void move(int x, int y, int[,] map)
+            {
+                if (x < 0 || y < 0 || x >= map.GetLength(0) || y >= map.GetLength(1)) return;
+
+                if (map[x, y] == 1) return;
+
+                this.x = x;
+                this.y = y;
+
+                px = x * w;
+                py = y * h;
+
+                pm.RenderTransform = new TranslateTransform(px, py);
+            }
+
+        }
+
+        public class CDir
+        {
+            public int x, y;
+            int dx, dy;
+
+            public CDir(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+                dx = 1; 
+                dy = 0;
+            }
+
+            public void update(int [,] map)
+            {
+                if ((x + dx < 0) || (x + dx >= map.GetLength(0)) || (y + dy < 0) || (y + dy > map.GetLength(1))) return;
+
+                x = x + dx;
+                y = y + dy;
+            }
+
+            public void up()
+            {
+                dy = 1;
+            }
+
+            public void down()
+            {
+                dy = -1;
+            }
+
+            public void left()
+            {
+                dx = -1;
+            }
+
+            public void right()
+            {
+                dx = 1;
+            }
+
+        }
+
+        CChar pakman;
+        CDir dir;
+        System.Windows.Threading.DispatcherTimer Timer;
+
+
+        public int[,] map = { 
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1 },
+            { 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1 },
+            { 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1 },
+            { 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1 },
+            { 1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1 },
+            { 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1 },
+            { 1, 1, 1, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 1, 1, 1 },
+            { 1, 2, 2, 2, 2, 1, 2, 1, 1, 0, 0, 1, 1, 2, 1, 2, 2, 2, 2, 1 },
+            { 1, 2, 1, 1, 2, 2, 2, 1, 0, 0, 0, 0, 1, 2, 2, 2, 1, 1, 2, 1 },
+            { 1, 2, 2, 1, 2, 1, 2, 1, 0, 0, 0, 0, 1, 2, 1, 2, 1, 2, 2, 1 },
+            { 1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1 },
+            { 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1 },
+            { 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1 },
+            { 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1 },
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } }; 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            pakman = new CChar(1, 1);
+            dir = new CDir(1, 1);
+
+            Timer = new System.Windows.Threading.DispatcherTimer();
+            Timer.Tick += new EventHandler(dispatcherTimer_Tick);
+            Timer.Interval = new TimeSpan(0, 0, 0, 1);
+          
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            dir.update(map);
+            pakman.move(dir.x, dir.y, map);
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            menu.Visibility = Visibility.Hidden;
+            Game.Visibility = Visibility.Visible;
+
+            Timer.Start();
+        }
+
+        private void Start_MouseEnter(object sender, MouseEventArgs e)
+        {
+            
+
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left) dir.left();
+        }
+    }
+
+   
+}
