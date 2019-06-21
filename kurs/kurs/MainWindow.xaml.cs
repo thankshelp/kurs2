673ff1cfbd601ecdc4ce3ba2a8fd12467ed03b57
thankshelp <taraninav.44@gmail.com>
2019-06-22 04:08:23 +0700

    add enemy

--- kurs/kurs/MainWindow.xaml.cs
+++ kurs/kurs/MainWindow.xaml.cs
@@ -20,7 +20,8 @@ namespace kurs
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+
+        int score;
         public class CChar
         {
             int x, y;
@@ -28,6 +29,11 @@ namespace kurs
 
             int h, w;
             Ellipse pm;
+            ImageBrush ib = new ImageBrush();
+            int currentFrame;
+            int currentRow;
+
+
 
             public CChar(int x, int y)
             {
@@ -36,17 +42,31 @@ namespace kurs
 
                 w = h = 45;
 
-                px = x * w; 
+                px = x * w;
                 py = y * h;
 
                 pm = new Ellipse();
 
-                pm.Stroke = Brushes.YellowGreen;
-                pm.Fill = Brushes.Yellow;
-                pm.StrokeThickness = 2;
+                //pm.Stroke = Brushes.YellowGreen;
+                //pm.Fill = Brushes.Yellow;
+                //pm.StrokeThickness = 2;
 
                 pm.Width = 45;
                 pm.Height = 45;
+
+                ib.AlignmentX = AlignmentX.Left;
+                ib.AlignmentY = AlignmentY.Top;
+                ib.Stretch = Stretch.None;
+
+                ib.Viewbox = new Rect(10, 10, 0, 0);
+                ib.ViewboxUnits = BrushMappingMode.Absolute;
+
+                currentFrame = 0;
+
+
+                ib.ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/image/pm (1).png", UriKind.Absolute));
+
+                pm.Fill = ib;
 
                 pm.RenderTransform = new TranslateTransform(px, py);
                 //pm.Margin = new Thickness(px, py, 0, 0);
@@ -58,19 +78,59 @@ namespace kurs
 
                 if (map[x, y] == 1) return true;
 
+
                 this.x = x;
                 this.y = y;
 
+                var frameCount = 8;
+                var frameW = 65;
+                var frameH = 65;
+
+
+
                 if (px > x * w)
-                    px-=1;
+                {
+                    px -= 1;
+                    currentFrame = (currentFrame + 1 + frameCount) % frameCount;
+                    currentRow = 1;
+
+                    var frameLeft = currentFrame * frameW;
+                    var frameTop = currentRow * frameH;
+                    (pm.Fill as ImageBrush).Viewbox = new Rect(frameLeft, frameTop + 10, frameLeft + frameW, frameTop + frameH);
+                }
                 if (px < x * w)
-                    px+=1;
+                {
+                    px += 1;
+                    currentFrame = (currentFrame + 1 + frameCount) % frameCount;
+                    currentRow = 0;
+
+                    var frameLeft = currentFrame * frameW;
+                    var frameTop = currentRow * frameH;
+                    (pm.Fill as ImageBrush).Viewbox = new Rect(frameLeft + 10, frameTop + 10, frameLeft + frameW, frameTop + frameH);
+                }
+
                 //px = x * w;
                 if (py > y * h)
-                    py-=1;
-                if (py < y * h)
-                    py+=1;
+                {
+                    py -= 1;
+                    currentFrame = (currentFrame + 1 + frameCount) % frameCount;
+                    currentRow = 2;
 
+                    var frameLeft = currentFrame * frameW;
+                    var frameTop = currentRow * frameH;
+                    (pm.Fill as ImageBrush).Viewbox = new Rect(frameLeft + 10, frameTop + 10, frameLeft + frameW, frameTop + frameH);
+                }
+
+                if (py < y * h)
+                {
+                    py += 1;
+                    currentFrame = (currentFrame + 1 + frameCount) % frameCount;
+                    currentRow = 3;
+
+                    var frameLeft = currentFrame * frameW;
+                    var frameTop = currentRow * frameH;
+                    (pm.Fill as ImageBrush).Viewbox = new Rect(frameLeft + 10, frameTop + 20, frameLeft + frameW, frameTop + frameH);
+                }
 
                 //py = y * h;
 
@@ -84,6 +144,80 @@ namespace kurs
             public void addToScene(ref Canvas scene)
             {
                 scene.Children.Add(pm);
+            }
+        }
+
+        public class Enemy
+        {
+            int x, y;
+            int px, py;
+
+            int h, w;
+            Rectangle ghost1, ghost2, ghost3;
+            ImageBrush ib = new ImageBrush();
+            int currentFrame;
+            int currentRow;
+
+            public Enemy(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+
+                w = h = 45;
+
+                px = x * w;
+                py = y * h;
+
+                ghost1 = new Rectangle();
+                ghost2 = new Rectangle();
+                ghost3 = new Rectangle();
+
+                ghost1.Stroke = Brushes.Purple;
+                ghost1.Fill = Brushes.Red;
+                ghost2.Stroke = Brushes.Purple;
+                ghost2.Fill = Brushes.Blue;
+                ghost3.Stroke = Brushes.Purple;
+                ghost3.Fill = Brushes.Green;
+                //pm.StrokeThickness = 2;
+                ghost1.HorizontalAlignment = HorizontalAlignment.Left;
+                ghost1.VerticalAlignment = VerticalAlignment.Center;
+                ghost2.HorizontalAlignment = HorizontalAlignment.Left;
+                ghost2.VerticalAlignment = VerticalAlignment.Center;
+                ghost3.HorizontalAlignment = HorizontalAlignment.Left;
+                ghost3.VerticalAlignment = VerticalAlignment.Center;
+
+                ghost1.Width = 45;
+                ghost1.Height = 45;
+                ghost2.Width = 45;
+                ghost2.Height = 45;
+                ghost3.Width = 45;
+                ghost3.Height = 45;
+
+                //ib.AlignmentX = AlignmentX.Left;
+                //ib.AlignmentY = AlignmentY.Top;
+                //ib.Stretch = Stretch.None;
+
+                //ib.Viewbox = new Rect(10, 10, 0, 0);
+                //ib.ViewboxUnits = BrushMappingMode.Absolute;
+
+                //currentFrame = 0;
+
+
+                //ib.ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/image/pm (1).png", UriKind.Absolute));
+
+                //pm.Fill = ib;
+
+                ghost1.RenderTransform = new TranslateTransform(px, py);
+                ghost2.RenderTransform = new TranslateTransform(px+45, py);
+                ghost3.RenderTransform = new TranslateTransform(px+90, py);
+
+
+            }
+            public void addToScene(ref Canvas scene)
+            {
+                scene.Children.Add(ghost1);
+                scene.Children.Add(ghost2);
+                scene.Children.Add(ghost3);
             }
         }
 
@@ -108,7 +242,13 @@ namespace kurs
                 {
                     x = x + dx;
                     y = y + dy;
+                    if (map[x + dx, y + dy] == 2)
+                    {
+                        map[x + dx, y + dy] = 0;
+                    }
                 }
+
+
             }
 
             public void up()
@@ -138,27 +278,52 @@ namespace kurs
         }
 
         CChar pakman;
+        Enemy gh1, gh2, gh3;
         CDir dir;
         System.Windows.Threading.DispatcherTimer Timer;
 
-        //20x16
-        public int[,] map = { 
-            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-            { 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1 },
-            { 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1 },
-            { 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1 },
-            { 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1 },
-            { 1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1 },
-            { 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1 },
-            { 1, 1, 1, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 1, 1, 1 },
-            { 1, 2, 2, 2, 2, 1, 2, 1, 1, 0, 0, 1, 1, 2, 1, 2, 2, 2, 2, 1 },
-            { 1, 2, 1, 1, 2, 2, 2, 1, 0, 0, 0, 0, 1, 2, 2, 2, 1, 1, 2, 1 },
-            { 1, 2, 2, 1, 2, 1, 2, 1, 0, 0, 0, 0, 1, 2, 1, 2, 1, 2, 2, 1 },
-            { 1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1 },
-            { 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1 },
-            { 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1 },
-            { 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1 },
-            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } }; 
+        //16x20
+
+        //{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+        //{ 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1 },
+        //{ 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1 },
+        //{ 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1 },
+        //{ 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1 },
+        //{ 1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 1, 2, 2, 2, 1, 2, 2, 2, 2, 1 },
+        //{ 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1 },
+        //{ 1, 1, 1, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 1, 1, 1 },
+        //{ 1, 2, 2, 2, 2, 1, 2, 1, 1, 0, 0, 1, 1, 2, 1, 2, 2, 2, 2, 1 },
+        //{ 1, 2, 1, 1, 2, 2, 2, 1, 0, 0, 0, 0, 1, 2, 2, 2, 1, 1, 2, 1 },
+        //{ 1, 2, 2, 1, 2, 1, 2, 1, 0, 0, 0, 0, 1, 2, 1, 2, 1, 2, 2, 1 },
+        //{ 1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1 },
+        //{ 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1 },
+        //{ 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1 },
+        //{ 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1 },
+        //{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } }; 
+
+       
+        public int[,] map = {
+                {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                {1,0,2,2,1,2,2,2,1,1,2,2,2,2,2,1},
+                {1,2,1,2,2,2,1,2,1,1,2,1,2,1,2,1},
+                {1,2,1,2,1,1,1,2,1,1,2,1,2,1,2,1},
+                {1,2,1,2,2,2,2,2,2,2,2,2,2,2,2,1},
+                {1,2,1,1,1,1,2,1,1,1,1,1,2,1,2,1},
+                {1,2,1,2,2,2,2,2,2,1,2,2,2,1,2,1},
+                {1,2,1,2,1,1,1,1,2,1,2,1,2,1,2,1},
+                {1,2,2,2,1,0,0,1,2,2,2,1,2,2,2,1},
+                {1,1,1,2,1,0,0,0,2,1,1,1,2,1,1,1},
+                {1,1,1,2,1,0,0,0,2,1,1,1,2,1,1,1},
+                {1,2,2,2,1,0,0,1,2,2,2,1,2,2,2,1},
+                {1,2,1,2,1,1,1,1,2,1,2,1,2,1,2,1},
+                {1,2,1,2,2,2,2,2,2,1,2,2,2,1,2,1},
+                {1,2,1,1,1,1,2,1,1,1,1,1,2,1,2,1},
+                {1,2,1,2,2,2,2,2,2,2,2,2,2,2,2,1},
+                {1,2,1,2,1,1,1,2,1,1,2,1,2,1,2,1},
+                {1,2,1,2,2,2,1,2,1,1,2,1,2,1,2,1},
+                {1,2,2,2,1,2,2,2,1,1,2,2,2,2,2,1},
+                {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                }; 
 
         public MainWindow()
         {
@@ -166,43 +331,60 @@ namespace kurs
 
             pakman = new CChar(1, 1);
 
+            gh1 = new Enemy(8, 5);
+            //gh2 = new Enemy(9, 5);
+            //gh3 = new Enemy(10, 5);
+
             dir = new CDir(1, 1);
 
             Timer = new System.Windows.Threading.DispatcherTimer();
             Timer.Tick += new EventHandler(dispatcherTimer_Tick);
             Timer.Interval = new TimeSpan(0, 0, 0, 0, 1);
-          
+             
+
             for (int i = 0; i < map.GetLength(0); i++)
                 for (int j = 0; j < map.GetLength(1); j++)
                 {
-                    Rectangle r = new Rectangle();
+                   Ellipse r = new Ellipse();
 
-                    r.Stroke = Brushes.YellowGreen;
-
-                    if (map[i,j] == 2 || map[i, j] == 0)
+                    if (map[i, j] == 2)
+                    {
                         r.Fill = Brushes.Azure;
-                    if (map[i, j] == 1)
-                        r.Fill = Brushes.Red;
+                        r.Width = 13;
+                        r.Height = 13;
+                    }
 
+                    if (map[i, j] == 3)
+                    {
+                        r.Fill = Brushes.Azure;
+                        r.Width = 20;
+                        r.Height = 20;
+                    }
 
                     r.StrokeThickness = 2;
 
-                    r.Width = 45;
-                    r.Height = 45;
+                      
 
-                    r.RenderTransform = new TranslateTransform(i * 45, j * 45);
+                   r.RenderTransform = new TranslateTransform(i * 45 + 15, j * 45 + 15);
                     Game.Children.Add(r);
                 }
 
             pakman.addToScene(ref Game);
+            gh1.addToScene(ref Game);
+            //gh2.addToScene(ref Game);
+            //gh3.addToScene(ref Game);
+
 
         }
 
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
-            
+
             if (pakman.move(dir.x, dir.y, map) == true)
+            {
                 dir.update(map);
+                //Game.Children.Remove(r);
+            }
         }
 
         private void Start_Click(object sender, RoutedEventArgs e)
